ccf0cc8f82e7321c76db53b4800501a272577325
Kambaralieva <Asemakambaralieva@gmail.com>
2020-08-09 16:00:11 +0600

    string instead of int

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,37 +3,42 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-      friday (24,10);
-      friday(15,20);
-      friday(46,12);
-
-
-
-
-
+        friday(24, 10);
+        friday(15, 20);
+        friday(46, 12);
+        friday(45,28);
+        friday(13,34);
 
 
     }
 
-    public static int friday(int ageOfperson, int temperature) {
+    public static String friday(int ageOfperson, int temperature) {
 
-
-        if ((ageOfperson >20 && ageOfperson <45) ||(temperature >-20 && temperature < 30)){
+        if ((ageOfperson > 20 && ageOfperson < 45) || (temperature > -20 && temperature < 30)) {
             System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
 
         }
-        if ((ageOfperson <20) ||(temperature >0 && temperature < 28)){
+        if ((ageOfperson < 20) || (temperature > 0 && temperature < 28)) {
             System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
 
         }
-        if ((ageOfperson > 45) || (temperature >-10 && temperature < 25)){
+        if ((ageOfperson > 45) || (temperature > -10 && temperature < 25)) {
             System.out.println("Оставайтесь дома");
+            return "Оставайтесь дома";
+
+        } else {
+            System.out.println("Оставайтесь дома");
+            return "Оставайтесь дома";
+
         }
-        return friday(ageOfperson, temperature);
+
 
     }
-// Алексей я старалась сделать как на видео на 47 минуте  там тольео иф . Не смогла понять
+
 }
+
 
 
 
